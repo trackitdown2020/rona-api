d3344c6cc833e7cc23f0b112887d7047998ee5af
Jeff Chen <jeffreychen006@gmail.com>
2020-05-22 21:46:19 -0700

    formatted data to be what the front-end API takes

--- controllers/covid19/mobility.js
+++ controllers/covid19/mobility.js
@@ -1,10 +1,31 @@
 const { queryMobility } = require('../../apis/covid19/mobilityAPI');
 
+const processMobilityDataHelper = (data) => {
+    console.log(data);
+    const timeValueMap = {};
+    data.forEach(({date, value}) => {
+        if(timeValueMap.hasOwnProperty(date)) {
+            timeValueMap.push(value);
+        } else {
+            timeValueMap[date] = [new Date(date), value];
+        }
+    });
+    const timeValuePoints = Object.values(timeValueMap);
+    const sortedTimeValuePoints = timeValuePoints.sort(([dateA], [dateB]) => (dateA - dateB));
+    return sortedTimeValuePoints;
+}
+
 const getMobility = async (req, res) => {
     let qs = req.query;
     const { country, state, type } = qs;
     const response = await queryMobility(country, state, type);
-    res.status(200).send(response);
+    if(response && response.points) {
+        const dataPoints = await processMobilityDataHelper(response.points);
+        res.status(200).send(dataPoints);
+    } else {
+        res.status(500).send({error: 'Issue getting mobility data.'})
+    }
 }
 
 module.exports = { getMobility };
+
