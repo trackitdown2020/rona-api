f880b4760d65365c794ee4a2914609bf78fcd589
djleng <dleng@usc.edu>
2020-05-28 22:23:30 -0700

    update venv

--- utils/formatters.js
+++ utils/formatters.js
@@ -3,13 +3,13 @@ const { upperCaseFirstLetter } = require('./string');
 const lineGraphFormatter = (data) => {
     const indexMap = {};
     const labels = ['Time'];
-    for(let [key, values] of Object.entries(data)) {
+    for (let [key, values] of Object.entries(data)) {
         labels.push(upperCaseFirstLetter(key));
         values.map((value, index) => {
-            if(indexMap.hasOwnProperty(index)) {
-                indexMap[index].push(value);
+            if (indexMap.hasOwnProperty(index)) {
+                indexMap[index].push(Math.floor(value));
             } else {
-                indexMap[index] = [index, value];
+                indexMap[index] = [index, Math.floor(value)];
             }
         })
 
