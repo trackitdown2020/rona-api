80a30f4e78a8a8e87b9097d290e4c96d19b7a5d1
Jeff Chen <jeffreychen006@gmail.com>
2020-08-30 17:09:49 -0700

    updated mapper

--- mappers/appleMobilityMapper.js
+++ mappers/appleMobilityMapper.js
@@ -2,45 +2,41 @@
 
 const appleMobilityMapper = (mobilityData) => {
     const { country, subregion, data } = mobilityData;
+    let max = Number.MIN_VALUE;
+    let min = Number.MAX_VALUE;
 
-    const driving = [];
-    const transit = [];
-    const walking = [];
-    let geoType = '';
+    const updatedData = data.map((dataPoint) => {
+        const { date, driving, transit, walking } = dataPoint;
 
-    data.forEach((dataPoint) => {
-        if(!geoType) {
-            geoType = dataPoint['geo_type'];
+        if(driving) {
+            max = Math.max(max, driving);
+            min = Math.min(min, driving);
         }
-        
-        const { date } = dataPoint;
-        if(dataPoint['driving']) {
-            driving.push({
-                x: new Date(date).getTime(),
-                y: dataPoint['driving']
-            });
+
+        if(transit) {
+            max = Math.max(max, transit);
+            min = Math.min(min, transit);
         }
-        if(dataPoint['transit']) {
-            transit.push({
-                x: new Date(date).getTime(),
-                y: dataPoint['transit']
-            });
+
+        if(walking) {
+            max = Math.max(max, walking);
+            min = Math.min(min, walking);
         }
-        if(dataPoint['walking']) {
-            walking.push({
-                x: new Date(date).getTime(),
-                y: dataPoint['walking']
-            });
+
+        return {
+            date: new Date(date).getTime(),
+            driving: driving && parseFloat(driving.toFixed(2)),
+            transit: transit && parseFloat(transit.toFixed(2)),
+            walking: walking && parseFloat(walking.toFixed(2))
         }
     });
 
     return {
         country,
         subregion,
-        geoType,
-        driving,
-        transit,
-        walking
+        max: Math.floor(Math.ceil((max + 20)/10) * 10),
+        min: Math.floor(Math.ceil((min + 20)/10) * 10),
+        data: updatedData
     }
 }
 
