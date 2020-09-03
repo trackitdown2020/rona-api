b1d6f78dd7aa96ee383afb49b682d8bb291743e5
Jeff Chen <jeffreychen006@gmail.com>
2020-09-03 01:16:03 -0700

    all working now

--- controllers/covid19/vaccine/index.js
+++ controllers/covid19/vaccine/index.js
@@ -1,9 +1,11 @@
 const { queryVaccineTrialData } = require('../../../apis/covid19/vaccine');
+const { vaccineTrialMapper } = require('../../../mappers/vaccineTrialMapper');
 
 const getVaccineTrialData = async (req, res) => {
     const response = await queryVaccineTrialData();
     if(response) {
-        res.status(200).send(response);
+        const mappedData = vaccineTrialMapper(response);
+        res.status(200).send(mappedData);
     } else {
         res.status(500).send({error: 'Issue getting vaccine trial data'});
     }
