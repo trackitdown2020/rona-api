81a7dbfa90d560fe566f9127bc132336dcc9cb8b
Jeff Chen <jeffreychen006@gmail.com>
2020-11-17 23:33:27 -0800

    Adds province summary and province query

--- controllers/covid19/summary.js
+++ controllers/covid19/summary.js
@@ -5,6 +5,10 @@ const {
     queryCountriesSummaryData,
     queryCountriesByCountrySummaryData
 } = require('../../apis/disease.sh/worldometer');
+const {
+    queryCountryProvinceHistorical,
+    queryCountryHistorical
+} = require('../../apis/disease.sh/jhucsse');
 
 const getWorldSummaryData = async (req, res) => {
     const response = await queryWorldSummaryData();
@@ -64,13 +68,71 @@ const getCountrySummaryByCountryData = async (req, res) => {
     }
 }
 
+const getProvinceSummaryByProvinceData = async (req, res) => {
+    const { country='', provinces='' } = req.params;
+    
+    if(!country) {
+        res.status(500).send({
+            error: `Could not query ${provinces} in unspecified country.`
+        });
+        return;
+    }
 
+    if(!provinces) {
+        res.status(500).send({
+            error: `Could not query unspecified provinces in ${country}.`
+        });
+        return;
+    }
+
+    const response = await queryCountryProvinceHistorical(country, provinces, 1);
+
+    if(response) {
+        const mappedProvinceData = response.map(provinceData => {
+            const { province, timeline } = provinceData;
+            const { cases, deaths, recovered } = timeline;
+            return {
+                province,
+                cases: Object.values(cases)[0],
+                deaths: Object.values(deaths)[0],
+                recovered: Object.values(recovered)[0]
+            }
+        });
+        res.status(200).send(mappedProvinceData);
+    } else {
+        res.status(500).send({
+            error: 'Could not process the data'
+        })
+    }  
+}
+
+const getCountryProvinces = async (req, res) => {
+    const { country } = req.params;
+
+    if(!country) {
+        res.status(500).send({
+            error: 'Could not query an unspecified country'
+        });
+    }
+
+    const response = await queryCountryHistorical(country, 1);
+
+    if(response) {
+        const { province } = response;
+        res.status(200).send(province);
+    } else {
+        res.status(500).send({
+            error: 'Could not process the data'
+        });
+    }
+}
 
 module.exports = {
     getWorldSummaryData,
     getUSSummaryData,
     getUSSummaryByStateData,
     getCountrySummaryData,
-    getCountrySummaryByCountryData
-
+    getCountrySummaryByCountryData,
+    getProvinceSummaryByProvinceData,
+    getCountryProvinces
 }
