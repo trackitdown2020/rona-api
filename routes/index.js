08efcb1916500c7f4c95361542f1bda8e1d2b498
Daniel Leng <42083011+dleng16@users.noreply.github.com>
2020-12-17 14:45:45 -0800

    remove data models

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ module.exports = {
   twitter: require('./twitter'),
   reddit: require('./reddit'),
   covid19: require('./covid19'),
-  dataModels: require("./dataModels"),
   country: require('./country'),
   geoJSON: require('./geojson'),
   youtube: require('./youtube')
