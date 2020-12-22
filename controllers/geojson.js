5e20b46dab97e01e0a9c734d0a01cc445c9eb267
Jeff Chen <jeffreychen006@gmail.com>
2020-12-21 22:16:03 -0800

    fixed geojson error

--- controllers/geojson.js
+++ controllers/geojson.js
@@ -1,13 +1,20 @@
 const axios = require('axios');
 
 const getCountryGeoJSON = async (req, res) => {
-    const { country } = req.params;
-    const { data } = await axios.get(`https://react-vector-maps.netlify.app/maps/${country}.json`);
-    if(data) {
-        res.status(200).send(data);
-    } else {
-        res.status(500).send(`Could not find geoJSON for ${country}`);
+    
+    try {
+        const { country } = req.params;
+        const { data } = await axios.get(`https://react-vector-maps.netlify.app/maps/${country}.json`);
+        if(data) {
+            res.status(200).send(data);
+        } else {
+            res.status(500).send({ error: `Could not find geoJSON for ${country}` });
+        }
+    } catch(error) {
+        res.status(500).send({ error })
     }
+  
+  
 }
 
 module.exports = {
